67f3cf282a00d185cc66a30256dcc16e37f52fd1
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-06-08 12:47:30 +0200

    scanner and download

--- theme-parkDOM.js
+++ theme-parkDOM.js
@@ -5,6 +5,29 @@ const buttonsElement = document.querySelector(".buttons")
 var parktransactions = parkTransactions()
 var QRid;
 
+function downloadImage() {
+ ;
+  if(localStorage.getItem("package") == "packageOne"){ 
+    var link = document.createElement('a')
+    link.href = '../one.png';
+  link.download = 'packageOne.png';
+  link.click();
+  }
+  else if(localStorage.getItem("package") == "packageTwo"){ 
+    var link = document.createElement('a')
+    link.href = '../two.png';
+  link.download = 'packageTwo.png';
+  link.click();
+  }
+  else if(localStorage.getItem("package") == "packageThree"){ 
+  var link = document.createElement('a')
+  link.href = '../three.png';
+  link.download = 'packageThree.png';
+  link.click();
+  }
+  
+}
+
 function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
     console.log(`Code matched = ${decodedText}`, decodedResult);
@@ -12,27 +35,33 @@ function onScanSuccess(decodedText, decodedResult) {
     var radioBtn = document.querySelector("input[name='packRadio']:checked");
   
   if(decodedText == "one"){
+      displayElement.style.display = "block"
       displayElement.innerHTML = "Your QR code is vailid, you're on package one"
       setTimeout(function () {
       displayElement.innerHTML = ""
+      displayElement.style.display = "none"
       }, 6000)
       buttonsElement.style.display = "flex";
       balanceElem.innerHTML = parktransactions.getPackageOne();
       QRid = decodedText;
     } 
   else if (decodedText == "two"){
+    displayElement.style.display = "block"
     displayElement.innerHTML = "Your QR code is vailid, you're on package two"
     setTimeout(function () {
     displayElement.innerHTML = ""
+    displayElement.style.display = "none"
     }, 6000)
     buttonsElement.style.display = "flex";
     balanceElem.innerHTML = parktransactions.getPackageTwo();
     QRid = decodedText;
   }
   else if(decodedText == "three"){
+    displayElement.style.display = "block"
     displayElement.innerHTML = "Your QR code is vailid, you're on package three"
     setTimeout(function () {
     displayElement.innerHTML = ""
+    displayElement.style.display = "none"
     }, 6000)
     buttonsElement.style.display = "flex";
     balanceElem.innerHTML = parktransactions.getPackageThree();
